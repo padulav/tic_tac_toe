413323b27249f9ba5eadb69e1c0f106f86283e18
Nick <moonscapegames_dev@gmail.com>
2022-04-03 00:33:00 +0100

    adding github link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import TicTacToe from './componets/TicTacToe'
+import github from './assets/github.png'
 
 class App extends React.Component {
   render() {return (
     <div className="App">
+
+      <a href="https://github.com/padulav/tic_tac_toe">
+          <img  src={github} alt='github source' className='github'/>
+      </a>
+      
 
       <TicTacToe/>
 
